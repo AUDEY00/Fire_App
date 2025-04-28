af10f0bdd33b1f91fe7529d5e05d10e5dee8fbae
AUDEY00 <202280098@psu.palawan.edu.ph>
2025-04-28 18:13:26 +0800

    adding charts multibar, multiline

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView,ChartView
+from fire.views import HomePageView,ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 from fire import views
 
 urlpatterns = [
@@ -9,6 +9,10 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('stations', views.map_station, name='map-station'),
     path('incidents', views.map_incident, name='map-incident'),
-    path ('dashboard chart', ChartView.as_view(), name='dashboard-chart'),
+    path ('dashboardchart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountbyMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
 
 ]
